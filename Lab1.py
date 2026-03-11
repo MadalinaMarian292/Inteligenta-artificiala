a5cf2049b46638f5fa3b5f0a0a37aeaab466621b
Robertd22 <162416644+Robertd22@users.noreply.github.com>
2026-03-11 17:47:25 +0200

    Lab1

---
+++ Lab1.py
@@ -0,0 +1,20 @@
+print(type(10))
+print (10+3)
+print (10-3)
+print (10*3)
+print(10/3)
+print(10%3)
+
+#string
+name='Madalina'
+print(name[2])
+print(name[:])
+print(name[2:])
+print('hello'.upper())
+
+#boolean
+bool(True)
+bool(False)
+print(bool(None))
+print(bool(False))
+print(bool(0))
