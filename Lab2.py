6a06980f87c080a858605b6800846017b690a49b
Madalina <madalinamarian61@gmail.com>
2026-03-18 16:11:43 +0200

    Lab2

---
+++ Lab2.py
@@ -0,0 +1,33 @@
+#list
+my_list = [1, 2, '8', 3, 7, 4, 3, 4, 1, 3, 3]
+print(len(my_list))
+print(my_list.index(4))
+print(my_list.count(3))
+print(my_list[5:])
+print(my_list[:5])
+print(my_list[:])
+print(my_list *2)
+
+#dictionary
+my_dict={'name':'Mada','age':22, 'magic_power': False }
+print(my_dict['name'])
+print(list(my_dict.keys()))
+print(list(my_dict.items()))
+print(list(my_dict.values()))
+
+#tuple
+my_tuple= ('banana', 'capsuni', 'portocala')
+print(len(my_tuple))
+print(my_tuple[2])
+print(my_tuple[-1])
+
+#set
+new_list= [1, 2, 4, 5, 7, 9]
+print(new_list)
+
+
+
+#none
+type(None)
+q=None
+print(q)
