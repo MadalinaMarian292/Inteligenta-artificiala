c4601a6b3119a94e6c3410320dd7a7f48c34a017
Madalina <madalinamarian61@gmail.com>
2026-04-08 15:58:19 +0300

    Lab4

--- Lab4.py
+++ Lab4.py
@@ -62,28 +62,190 @@
 
 
 #exercitiul 2
-def genereaza_factura(nume_client, **produse):
-    print("===== FACTURA =====")
-    print(f"Client: {nume_client}")
-    print("-------------------")
+# def genereaza_factura(nume_client, **produse):
+#     print("===== FACTURA =====")
+#     print(f"Client: {nume_client}")
+#     print("-------------------")
+#
+#     total = 0
+#
+#     for produs, pret in produse.items():
+#         print(f"{produs}: {pret} lei")
+#         total += pret
+#
+#     print("-------------------")
+#     print(f"Total de plata: {total} lei")
+#     print("===================")
+#
+#
+#
+# genereaza_factura(
+#     "Madalina",
+#     paine=5,
+#     lapte=7,
+#     oua=12
+# )
 
-    total = 0
 
-    for produs, pret in produse.items():
-        print(f"{produs}: {pret} lei")
-        total += pret
+#exercitiul 3
+# def normalize_data(data):
+#
+#     if not data:
+#         return []
+#
+#     min_val = min(data)
+#     max_val = max(data)
+#
+#     if min_val == max_val:
+#         return [0.0 for _ in data]
+#
+#     normalized = []
+#     for x in data:
+#         x_norm = (x - min_val) / (max_val - min_val)
+#         normalized.append(x_norm)
+#
+#     return normalized
+#
+# data = [10, 20, 30, 40, 50]
+# normalized_data = normalize_data(data)
+#
+# print(normalized_data)
 
-    print("-------------------")
-    print(f"Total de plata: {total} lei")
-    print("===================")
+
+#exercitiul 4
+# square_list = lambda lst: [x**2 for x in lst]
+#
+# print(square_list([1, 2, 3]))
+
+#exercitiul 5
+# a = [(0, 2), (4, 3), (9, 9), (10, -1)]
+#
+# sorted_a = sorted(a, key=lambda x: x[1])
+#
+# print(sorted_a)
 
 
+#exercitiul 6
+# def get_even_numbers(numbers):
+#     return list(filter(lambda x: x % 2 == 0, numbers))
+#
+# def get_odd_numbers(numbers):
+#     return list(filter(lambda x: x % 2 != 0, numbers))
+#
+#
+# orig_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#
+# even_list = get_even_numbers(orig_list)
+# odd_list = get_odd_numbers(orig_list)
+#
+# print("Lista originala:", orig_list)
+# print("Numere pare:", even_list)
+# print("Numere impare:", odd_list)
 
-genereaza_factura(
-    "Madalina",
-    paine=5,
-    lapte=7,
-    oua=12
-)
 
+#exercitiul 7
+# def remove_none(prices):
+#     return list(filter(lambda x: x is not None, prices))
+#
+# def apply_discount(prices):
+#     return list(map(lambda x: x * 0.9, prices))
+#
+#
+# prices = [100, 200, None, 50, None, 300]
+#
+# valid_prices = remove_none(prices)
+# discounted_prices = apply_discount(valid_prices)
+#
+# print("Lista de preturi initiale:", prices)
+# print("Fara None:", valid_prices)
+# print("Cu reducerea de 10%:", discounted_prices)
+
+#exercitiul 8
+# from datetime import datetime
+#
+# dt = datetime(2004, 4, 1, 9, 3, 32, 744178)
+#
+#
+# get_year = lambda x: x.year
+# get_month = lambda x: x.month
+# get_day = lambda x: x.day
+# get_time = lambda x: x.time()
+#
+#
+# print(dt)
+# print(get_year(dt))
+# print(get_month(dt))
+# print(get_day(dt))
+# print(get_time(dt))
+
+#exercitiul 9
+# def sum_lists(list1, list2):
+#     return [a + b for a, b in zip(list1, list2)]
+#
+#
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [10, 20, 30, 40, 50]
+#
+# result = sum_lists(list1, list2)
+#
+# print(result)
+
+#exercitiul 10
+# even_numbers = [x for x in range(0, 101) if x % 2 == 0]
+#
+# cubes = [x**3 for x in range(1, 11)]
+#
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [3, 4, 5, 6, 7]
+#
+# common = [x for x in list1 if x in list2]
+#
+# print("Numerele pare (0-100):")
+# print(even_numbers)
+#
+# print("\nCuburile primelor 10 numere:")
+# print(cubes)
+#
+# print("\nElementele comune:")
+# print(common)
+
+#exercitiul 11
+
+# even_set = {x for x in range(0, 20) if x % 2 == 0}
+#
+#
+# text = "programare python"
+# letters_set = {ch for ch in text if ch != " "}
+#
+# sentence = "Invatam programare in Python foarte usor"
+# words_set = {word for word in sentence.split() if len(word) >= 5}
+#
+#
+# print("Set numere pare:")
+# print(even_set)
+#
+# print("\nLiterele distincte:")
+# print(letters_set)
+#
+# print("\nCuvinte de minim 5 litere:")
+# print(words_set)
+
+
+#exercitiul 12
+
+squares_dict = {x: x**2 for x in range(1, 11)}
+
+text = "programare python"
+letters_count = {ch: text.count(ch) for ch in text if ch != " "}
+
+divisors_dict = {x: [i for i in range(1, x+1) if x % i == 0] for x in range(1, 11)}
+
+print("Numerele și pătratele lor:")
+print(squares_dict)
+
+print("\nNumere apariții litere:")
+print(letters_count)
+
+print("\nNumerele și lista divizorilor:")
+print(divisors_dict)
 
