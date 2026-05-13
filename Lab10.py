17a3a1097a2f2036bc813a709f9b973e9d1a119e
MadalinaMarian292 <madalinamarian61@gmail.com>
2026-05-13 16:02:48 +0300

    Lab10

---
+++ Lab10.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from pandas import set_option
+
+# studenti=pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti)
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.head())
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.info())
+
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.describe())
+
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.isnull().sum())
+
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.select_dtypes(include='object').columns)
+
+
+
+# studenti = pd.read_csv('StudentsPerformance.csv')
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# print(studenti.select_dtypes(include=['int64', 'float64']).columns)
+
+
+
+
+
+
